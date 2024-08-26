693c20a837e282bed44f2529439049772d14bd3b
jaiprashanth99x <jaiprashanthr@99x.io>
2024-08-26 12:14:21 +0800

    Added new project and reached to NuGet Packages for Entity Framework

---
+++ MirageResort.Domain/Entities/Villa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirageResort.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occcupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
