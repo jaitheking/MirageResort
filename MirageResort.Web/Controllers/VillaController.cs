caab64a90f47d5543c114f017981c0156fdb8454
jaiprashanth99x <jaiprashanthr@99x.io>
2024-08-27 00:07:25 +0800

    Section 3 done

---
+++ MirageResort.Web/Controllers/VillaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MirageResort.Domain.Entities;
+using MirageResort.Infrastructure.Data;
+namespace MirageResort.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if(obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "Name and Description cannot be the same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+            
+        }
+    }
+}
